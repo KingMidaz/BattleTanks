a266a082f1a414500e12aa0a9a8a964b25427799
THEMIDASMACHINE\Dscragg <dscragg@live.com>
2017-06-17 23:49:57 +0200

    Version_9

--- BattleTanks/BattleTanks/Source/BattleTanks/TankPlayerController.cpp
+++ BattleTanks/BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -2,6 +2,23 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"));
+
+	ATank* Tank = GetControlledTank();
+
+	if (Tank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is being controlled"), *Tank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No controlled tank found"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
