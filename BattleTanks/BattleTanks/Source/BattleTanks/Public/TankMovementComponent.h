9e7fce98d1129adee8bda41c79bb0167b30e3da3
THEMIDASMACHINE\Dscragg <dscragg@live.com>
2017-09-02 12:22:33 +0200

    Version_29

--- BattleTanks/BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
+++ BattleTanks/BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
@@ -1,5 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright Midas Development Studios
 #pragma once
 
 #include "CoreMinimal.h"
@@ -27,10 +26,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 
-	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 	
+	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 };
